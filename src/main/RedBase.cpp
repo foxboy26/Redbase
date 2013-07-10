36d4032a703bc387d11294e3bcb2c693df25a622
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2013-07-11 06:44:35 +0800

    Remove RedBase.h

--- src/main/RedBase.cpp
+++ src/main/RedBase.cpp
@@ -1,7 +1,14 @@
-#include "RedBase.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  cout << "Welcome to Redbase" << endl;
+
+
+
+  cout << "Bye!" << endl;
 
   return 0;
 }
