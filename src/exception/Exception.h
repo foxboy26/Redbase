338cc04f55fdc9c5398d4264f4a316219106230c
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2013-07-11 22:32:00 -0700

    Bug fix in exception package

--- src/exception/Exception.h
+++ src/exception/Exception.h
@@ -1,27 +1,22 @@
-#include <exception>
-#include <cstdint>
-#include <sstream>
+#pragma once
 
-/* for test only */
-#include <iostream>
-using namespace std;
+#include <exception>
+#include <sstream>
 
 class Exception : public std::exception
 {
-public:
-  virtual const char* what() const throw();
-protected:
-  std::string PrintMessage(const char* module, const char* message) const;
+  public:
+    virtual const char* what() const throw();
+  protected:
+    std::string PrintMessage(const char* module, const char* message) const
+    {
+      std::stringstream ss;
+      ss << "Exception: [" << module << "] " << message;
+      return ss.str();
+    }
 };
 
 const char* Exception::what() const throw()
 {
   return "General exception";
-}
-
-std::string Exception::PrintMessage(const char* module, const char* message) const
-{
-  std::stringstream ss;
-  ss << "Exception: [" << module << "] " << message;
-  return ss.str();
 }
--- src/exception/PF_Exception.h
+++ src/exception/PF_Exception.h
@@ -1,10 +1,6 @@
+#pragma once
+
 #include "Exception.h"
-
-#include <sstream>
-
-/* for test only */
-#include <iostream>
-using namespace std;
 
 class PF_Exception : public Exception
 {
@@ -20,38 +16,38 @@ class PF_Exception : public Exception
       CLOSEDFILE
     };
 
-    PF_Exception(PF_Exception::RC rc) : rc(rc) {}
+    PF_Exception(PF_Exception::RC _rc) : rc(_rc) {}
 
     const char* what() const throw();
 
   private:
+    static constexpr const char* module = "PF";
+    static constexpr const char* message[] = 
+    {
+      "page pinned in buffer",
+      "page to be unpinned is not in buffer",
+      "page already unpinned",
+      "page already free",
+      "invalid page number",
+      "file handle already open",
+      "file is closed",
+      "no memory",
+      "no buffer space",
+      "incomplete read of page from file",
+      "incomplete write of page to file",
+      "incomplete read of header from file",
+      "incomplete write of header from file",
+      "new page to be allocated already in buffer",
+      "hash table entry not found",
+      "page already in hash table",
+      "invalid file name"
+    };
+
     RC rc;
-    static const char* const module;
-    static const char* const message[];
 };
 
-const char* const PF_Exception::module = "PF";
-
-const char* const PF_Exception::message[] = 
-{
-  "page pinned in buffer",
-  "page to be unpinned is not in buffer",
-  "page already unpinned",
-  "page already free",
-  "invalid page number",
-  "file handle already open",
-  "file is closed",
-  "no memory",
-  "no buffer space",
-  "incomplete read of page from file",
-  "incomplete write of page to file",
-  "incomplete read of header from file",
-  "incomplete write of header from file",
-  "new page to be allocated already in buffer",
-  "hash table entry not found",
-  "page already in hash table",
-  "invalid file name"
-};
+constexpr const char* PF_Exception::module;
+constexpr const char* PF_Exception::message[];
 
 const char* PF_Exception::what() const throw()
 {
